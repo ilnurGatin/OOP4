052f512b749794c863ee04eff45b289bfe8ceda9
ilnurGatin <reilag@yandex.ru>
2022-10-26 02:28:18 +0500

    Task 2

--- src/Driver.java
+++ src/Driver.java
@@ -6,7 +6,6 @@ public class Driver <A extends Transport> {
     private A car;
 
 
-
     public Driver(String fullName, String typeOfDriverLicense, int yearsOfExperience, A car) {
         setFullName(fullName);
         setTypeOfDriverLicense(typeOfDriverLicense);
@@ -19,44 +18,47 @@ public class Driver <A extends Transport> {
     }
 
     public void setTypeOfDriverLicense(String typeOfDriverLicense) {
-        this.typeOfDriverLicense = ValidationUtils.validOrDefault(typeOfDriverLicense, "B");
+        if (typeOfDriverLicense == null || typeOfDriverLicense.isEmpty() || typeOfDriverLicense.isBlank()) {
+            throw new IllegalArgumentException("Необходимо указать тип прав!");
+        } else {
+            this.typeOfDriverLicense = typeOfDriverLicense;
+        }
     }
+        public String getFullName () {
+            return fullName;
+        }
 
-    public String getFullName() {
-        return fullName;
+        public void setFullName (String fullName){
+            this.fullName = ValidationUtils.validOrDefault(fullName, "Ivanov Ivan Ivanovich");
+        }
+
+
+        public int getYearsOfExperience () {
+            return yearsOfExperience;
+        }
+
+        public void setYearsOfExperience ( int yearsOfExperience){
+            this.yearsOfExperience = Math.max(yearsOfExperience, 0);
+        }
+
+        public A getCar () {
+            return car;
+        }
+
+        public void startMoving () {
+            System.out.println("Начал движение");
+        }
+
+        public void stopped () {
+            System.out.println(" Остановился");
+        }
+
+        public void refillVehicle () {
+            System.out.println("Заправил " + this.getCar().getBrand() + this.getCar().getModel());
+        }
+
+        //@Override
+        //public String toString() {
+        //    return "Водитель " + fullName + " управляет автомобилем " + this.car.getBrand() + this.car.getModel() + " и будет участвовать в заезде";
+        //}
     }
-
-    public void setFullName(String fullName) {
-        this.fullName = ValidationUtils.validOrDefault(fullName, "Ivanov Ivan Ivanovich");
-    }
-
-
-    public int getYearsOfExperience() {
-        return yearsOfExperience;
-    }
-
-    public void setYearsOfExperience(int yearsOfExperience) {
-        this.yearsOfExperience = Math.max(yearsOfExperience, 0);
-    }
-
-    public A getCar() {
-        return car;
-    }
-
-    public void startMoving() {
-        System.out.println("Начал движение");
-    }
-
-    public void stopped() {
-        System.out.println(" Остановился");
-    }
-
-    public void refillVehicle() {
-        System.out.println("Заправил " + this.getCar().getBrand() + this.getCar().getModel());
-    }
-
-    //@Override
-    //public String toString() {
-    //    return "Водитель " + fullName + " управляет автомобилем " + this.car.getBrand() + this.car.getModel() + " и будет участвовать в заезде";
-    //}
-}
